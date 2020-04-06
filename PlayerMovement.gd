0b68b480e2d7ebd648e22d5294a5fcdc18b6aa20
Agustín Marsero <amarsero@gmail.com>
2020-04-06 09:46:16 -0300

    Added writter!

--- PlayerMovement.gd
+++ PlayerMovement.gd
@@ -6,6 +6,7 @@ export (int) var accel = 75
 export (int) var jump_speed = -400
 export (int) var gravity = 1200
 export (int) var friction = 20
+export(int, "Writter", "Golden", "Silver") var character
 
 var velocity = Vector2()
 var jumping = false
@@ -13,12 +14,26 @@ var right = false
 var left = false
 var jump = false
 
+#var hola = false
+
+var right_input
+var left_input
+var jump_input
+
 onready var _sprite = $Sprite
 
+func _ready():
+	set_inputs()
+	
+func set_inputs():
+	right_input = "player" + str(character) + "_right"
+	left_input = "player" + str(character) + "_left"
+	jump_input = "player" + str(character) + "_jump"
+
 func get_input():
-	right = Input.is_action_pressed('move_right')
-	left = Input.is_action_pressed('move_left')
-	jump = Input.is_action_pressed('move_fowards')
+	right = Input.is_action_pressed(right_input)
+	left = Input.is_action_pressed(left_input)
+	jump = Input.is_action_pressed(jump_input)
 
 	if !jumping and jump and is_on_floor():
         jumping = true
@@ -32,7 +47,7 @@ func get_input():
 		if velocity.x < -run_speed:
 			velocity.x = -run_speed
 	
-func friction():
+func add_friction():
 	if is_on_floor():
 		if !(right || left) && velocity.length() > friction*2:
 			velocity = Vector2(0,0)
@@ -41,16 +56,18 @@ func friction():
 			velocity.y += sign(velocity.y) * -friction
 
 func _physics_process(delta):
+	
+#	if !is_on_floor():
 	velocity.y += gravity * delta
-	friction()
+	add_friction()
 	get_input()
 	if jumping and is_on_floor():
 		jumping = false
 		
 	if velocity.x > 0:
-		_sprite.scale.x = 1.0
+		_sprite.flip_h = false
 	elif velocity.x < 0:
-		_sprite.scale.x = -1.0
+		_sprite.flip_h = true
 	
 #	if hola:
 #		print(velocity)
